f5f90dc928f27e91e4b8ec7e301f059600f9c7e1
Михаил <apelsin14vorontsov@yandex.ru>
2023-03-26 13:37:55 +0500

    FlaskProject

--- mod4/flask_wtform.py
+++ mod4/flask_wtform.py
@@ -18,16 +18,21 @@ class RegistrationForm(FlaskForm):
 def number_length(min: int, max: int, message: Optional[str] = None):
 
     def _number_length(form: FlaskForm, field: Field):
-        if field.data < 1000000000 or field.data > 9999999999:
-            raise ValidationError
+        if len(str(field.data)) < min or len(str(field.data)) > max or field.data <= 0:
+            raise ValidationError(massage=message)
 
     return _number_length
 
 
 class NumberLength:
+    def __init__(self, min: int, max: int, message: Optional = ''):
+        self.min = min
+        self.max = max
+        self.massage = message
+
     def __call__(self, form: FlaskForm, field: Field):
-        if field.data < 1000000000 or field.data > 9999999999:
-            raise ValidationError
+        if len(str(field.data)) < self.min or len(str(field.data)) > self.max or field.data <= 0:
+            raise ValidationError(message=self.massage)
 
 
 number = IntegerField(validators=[InputRequired(), NumberLength()])
